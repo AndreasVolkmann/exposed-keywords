f0fafc5f9d6f5ba1bafa007860c62e85e3e19c3d
avo <avo@apurebase.com>
2018-02-08 19:22:24 +0100

    test without exposed and filter against official lists

--- src/main/kotlin/me/avo/exposed/Main.kt
+++ src/main/kotlin/me/avo/exposed/Main.kt
@@ -2,21 +2,39 @@ package me.avo.exposed
 
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.*
+import java.sql.*
 
 object table : Table("table") {
     val index = integer("index")
 }
 
+const val url = "jdbc:mysql://localhost/exposed"
+const val driver = "org.mariadb.jdbc.Driver"
+const val user = "test"
+const val password = "test"
+
 fun main(args: Array<String>) {
-    Database.connect(
-        driver = "org.mariadb.jdbc.Driver",
-        url = "jdbc:mysql://localhost/exposed",
-        user = "test",
-        password = "test"
-    )
+    Class.forName(driver).newInstance()
+    val con = DriverManager.getConnection(url, user, password)
+    val dbKeywords = con.metaData.sqlKeywords.split(",")
+    con.close()
+    val keywordsFromWeb = loadFileLines("reserved-keywords-from-web.txt")
+    val sql2003Keywords = loadFileLines("sql2003-keywords.txt")
+
+    keywordsFromWeb
+        .filterNot(dbKeywords::contains)
+        .filterNot(sql2003Keywords::contains)
+        .forEach(::println)
+}
+
+fun loadFileLines(name: String) = Main::class.java.classLoader.getResource(name).readText().split("\n")
+
+fun checkExposed() {
+    Database.connect(driver = driver, url = url, user = user, password = password)
 
     transaction {
         SchemaUtils.create(table)
     }
 }
 
+object Main
